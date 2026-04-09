9e3ff69a775374663ce84dfca75fcf2d8a9674ed
Leonardo Quinones <leito25@users.noreply.github.com>
2026-04-09 16:56:29 -0500

    Refactor API token handling and base URL construction

--- StreamLiteCourse/fast_kickoff/app.py
+++ StreamLiteCourse/fast_kickoff/app.py
@@ -79,11 +79,11 @@ email = "leonardo.quinones@unity3d.com"
 # It's best practice to store your API token securely, e.g., as an environment variable
 # For this example, we'll use a placeholder, but in a real application, use os.getenv()
 # FROm Streamlit cloud 
-api_token = st.secrets.get("ZENDESK_API_KEY")#, os.getenv("ZENDESK_API_KEY", "MY_ZENDESK_KEY"))
-#api_token = os.getenv("MY_ZENDESK_KEY")#, "MY_ZENDESK_KEY")
+#api_token = st.secrets.get("ZENDESK_API_KEY")#, os.getenv("ZENDESK_API_KEY", "MY_ZENDESK_KEY"))
+api_token = os.getenv("MY_ZENDESK_KEY")#, "MY_ZENDESK_KEY")
 
 # Construct the combined string
-combined_str = f"{email}/token:{api_token}"
+combined_str = (f"{EMAIL}/token", api_token)
 
 # Encode the string in Base64
 encoded_bytes = base64.b64encode(combined_str.encode("utf-8"))
@@ -95,6 +95,10 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# Assuming 'headers' is already defined from the authentication snippet above
+#base_url = "https://unity3d1757688765.zendesk.com" #unity3d.zendesk.com #https://unity3d1757688765.zendesk.com
+base_url = f"https://{SUBDOMAIN}.zendesk.com"
+st.title("Zendesk Tickets Dashboard")
 
 # Example validation (optional)
 if len(zd_base64_encoded_str) == 92:
@@ -104,10 +108,7 @@ else:
     print('Issues with Zendesk Token Validation')
     st.error('Issues with Zendesk Token Validation')
 
-# Assuming 'headers' is already defined from the authentication snippet above
-#base_url = "https://unity3d1757688765.zendesk.com" #unity3d.zendesk.com #https://unity3d1757688765.zendesk.com
-base_url = f"https://{SUBDOMAIN}.zendesk.com"
-st.title("Zendesk Tickets Dashboard")
+
 
 # Create tabs
 tab1, tab2, tab3, tab4 = st.tabs(["📋 Fetch Tickets", "📊 Status Timeline", "📄 Tickets Status Details", "🤖 Sentiment Analysis"])
